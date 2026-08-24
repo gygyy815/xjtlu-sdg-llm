94ce10f8baac92a9c86b394f5991f553fabe4b58
gygyy815 <G810443527@outlook.com>
2026-08-25 02:56:56 +0800

    style: load readable bilingual green UI v5

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import "./friendly-green.css";
 import "./friendly-green-v2.css";
 import "./friendly-green-v3.css";
 import "./friendly-green-v4.css";
+import "./friendly-green-v5.css";
 import "../components/KnowledgeGraphCard.css";
 import { UiLanguageToggle } from "@/components/UiLanguageToggle";
 import { EvaluationShortcut } from "@/components/EvaluationShortcut";
