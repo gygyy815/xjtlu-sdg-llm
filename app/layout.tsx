ab31f70be108d42d3c424f12fcd0562f69d90046
gygyy815 <G810443527@outlook.com>
2026-08-25 02:43:40 +0800

    style: load hardened green UI v4

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import "./evidence-ui-polish.css";
 import "./friendly-green.css";
 import "./friendly-green-v2.css";
 import "./friendly-green-v3.css";
+import "./friendly-green-v4.css";
 import "../components/KnowledgeGraphCard.css";
 import { UiLanguageToggle } from "@/components/UiLanguageToggle";
 import { EvaluationShortcut } from "@/components/EvaluationShortcut";
