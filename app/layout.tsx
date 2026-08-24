67e3bc25f457fd36c9ced112b5e18572495cbc8b
gygyy815 <G810443527@outlook.com>
2026-08-24 21:07:00 +0800

    style: load green campus layout v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import "./responsive-hotfix.css";
 import "./visual-polish.css";
 import "./evidence-ui-polish.css";
 import "./friendly-green.css";
+import "./friendly-green-v2.css";
 import "../components/KnowledgeGraphCard.css";
 import { UiLanguageToggle } from "@/components/UiLanguageToggle";
 import { EvaluationShortcut } from "@/components/EvaluationShortcut";
